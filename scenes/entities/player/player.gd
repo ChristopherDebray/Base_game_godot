b38049ea93102e067eb41943b40fb88b0b8a1e5b
Christopher Debray <christopherdebray1@gmail.com>
2026-09-06 20:32:12 +0200

    feat: added base for facing and animation update on player

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,10 +1,21 @@
 extends CharacterBody2D
 
+class_name Player
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var aim_component: Node2D = $AimComponent
+@onready var muzzle: Node2D = $Muzzle
 
 const SPEED: float = 130.0
 const PROBE_SIZE = Vector2(50, 50)
+const MUZZLE_INVERTION_POS: float = -10
+
+var facing_direction: Vector2 = Vector2.RIGHT
+var muzzle_initial_position: float = 27
+var idle_anim_name := "idle" 
+var run_anim_name := "walk" 
+var idle_frame_index := 1
 
 func _ready() -> void:
 	return
@@ -15,7 +26,8 @@ func _physics_process(delta: float) -> void:
 	
 	get_movement_input()
 	move_and_slide()
-	rotation = velocity.angle()
+	_update_facing()
+	_update_anim()
 
 func get_movement_input() -> void:
 	var nv: Vector2 = Vector2.ZERO
@@ -23,3 +35,32 @@ func get_movement_input() -> void:
 	nv.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	nv.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	velocity = nv.normalized() * SPEED
+
+func get_aim_direction() -> Vector2:
+	return global_position.direction_to(get_aim_world_position())
+
+func get_aim_world_position() -> Vector2:
+	return aim_component.get_aim_world_position()
+
+func _update_facing() -> void:
+	var aim_dir := get_aim_direction()
+
+	if aim_dir.x < -0.05:
+		animated_sprite_2d.flip_h = true
+		muzzle.position.x = MUZZLE_INVERTION_POS
+		facing_direction = Vector2.LEFT
+	elif aim_dir.x > 0.05:
+		animated_sprite_2d.flip_h = false
+		muzzle.position.x = muzzle_initial_position
+		facing_direction = Vector2.RIGHT
+
+func _update_anim() -> void:
+	# seuil pour éviter de “jouer/arrêter” quand la vitesse est quasi nulle
+	var moving := velocity.length_squared() > 1.0
+	#move_toward()
+
+	if moving:
+		if animated_sprite_2d.animation != run_anim_name or !animated_sprite_2d.is_playing():
+			animated_sprite_2d.play(run_anim_name)
+	else:
+		animated_sprite_2d.play(idle_anim_name)
